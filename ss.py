ce789860b823ce943cdebe411d8dca2495d3a2e0
Shubham71 <31090407+Shubham71@users.noreply.github.com>
2017-08-31 11:26:48 +0530

    Add files via upload

---
+++ ss.py
@@ -0,0 +1,8 @@
+avg=int(input("enter the numbers"))
+sum=0
+for i in range(1,avg+1):
+    x=int(input("Enter the numbers: "))
+    sum=sum+x
+average=float(sum/avg)
+print("The average is",average)
+    
